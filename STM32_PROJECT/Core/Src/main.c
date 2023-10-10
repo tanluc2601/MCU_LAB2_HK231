75210d454a88d6bd02c2f3639315eb6e271f3711
tanluc2601 <tantanluc08@gmail.com>
2023-10-10 22:01:12 +0700

    add ex3-10 (stm32) to ex3-10

--- STM32_PROJECT/Core/Src/main.c
+++ STM32_PROJECT/Core/Src/main.c
@@ -98,10 +98,9 @@ int main(void)
   setTimer1(100);
   while (1)
   {
-	  // This is main exercise
+	  // This is exercise 3 to exercise 10
 	  if (timer1_flag == 1){
-		  setTimer1(200);
-		  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+
 	  }
     /* USER CODE END WHILE */
 
